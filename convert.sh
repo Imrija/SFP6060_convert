8c761a7aa1c3e7cb991c9da8756bb263c895a75a
Jan Imrich <iimrija@gmail.com>
2014-12-25 12:13:54 +0000

    first working commit

--- convert.sh
+++ convert.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+for i in input/*; 
+do 
+	filename=$(basename "$i")
+	./ffmpegAMV -i "$i" -f amv -s 128x128 -r 16 -ac 1 -ar 22050 -qmin 3 -qmax 3 output/"${filename%.*}".amv
 
-ls
+done;
+
