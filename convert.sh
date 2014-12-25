9d4cd7a350f617d7f75477a27dd6d8fd9a4c8945
Jan Imrich <iimrija@gmail.com>
2014-12-25 12:22:51 +0000

    added directory checking

--- convert.sh
+++ convert.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-for i in input/*; 
-do 
-	filename=$(basename "$i")
-	./ffmpegAMV -i "$i" -f amv -s 128x128 -r 16 -ac 1 -ar 22050 -qmin 3 -qmax 3 output/"${filename%.*}".amv
+if [ ! -d "input" ]
+then
+	echo "input directory not found, creating one"
+	mkdir input
+fi
 
-done;
+if [ ! -d "output" ]
+then
+	echo "output directory not found, creating one"
+	mkdir output
+fi
 
+if [ "$(ls -A "input")" ] 
+then
+
+	for i in input/*; 
+	do 
+		filename=$(basename "$i")
+		./ffmpegAMV -i "$i" -f amv -s 128x128 -r 16 -ac 1 -ar 22050 -qmin 3 -qmax 3 output/"${filename%.*}".amv
+
+	done;
+else
+	echo "input directory is empty :("
+fi
